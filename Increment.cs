989b8a6454bfd944c5527eb8ca2f480c8081700b
FelixSedney <30689523+FelixSedney@users.noreply.github.com>
2020-10-27 12:03:45 +0100

    Add files via upload

---
+++ Increment.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 123;
+            a=a + 1;
+            a += 1;
+            a++;//post increment
+            ++a;//pre increment
+
+            
+            a = 123;
+            int b = a++;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            a = 123;
+            b = ++a;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            Console.WriteLine(a==124);
+            int x = 0;
+            if (x>0 & b / x > 12) { Console.WriteLine("whatever"); }
+        }
+    }
+}
---
+++ IterationDemo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace IterationDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 11; i++)//gebruiken als het aantal iteraties van te voren bekend is.
+            {
+                Console.WriteLine($"{i} x 4 = {i * 4}");
+            }
+            //Console.WriteLine(i);
+
+            //Console.WriteLine("please enter your password");
+
+            //string pwd = Console.ReadLine();
+            //voorwaarde vooraf
+            //while (pwd != "sesamopen")//gebruiken als aantal iteraties niet bekend is en de code 0 of meerdere keren doorlopen moet worden
+
+            //{
+            //    Console.WriteLine("please enter your password");
+            //    pwd = Console.ReadLine();
+            //}
+
+            string pwd;
+            //voorwaarde achteraf
+            do//aantal iteraties is niet van te voren bekend, maar is minimaal 1
+            {
+                Console.WriteLine("please enter your password");
+                pwd = Console.ReadLine();
+            } while (pwd != "sesamopen");
+
+            int y = 1;
+
+            while (y < 11)
+            {
+                Console.WriteLine($"{y} x 4 = {y * 4}");
+                y++;
+            }
+
+        }
+    }
+}
