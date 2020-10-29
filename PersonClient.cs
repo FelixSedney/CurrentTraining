abc86be253fc003edb8b95c51373fcccaff69d3c
FelixSedney <30689523+FelixSedney@users.noreply.github.com>
2020-10-29 14:31:22 +0100

    Add files via upload

---
+++ PersonClient.cs
@@ -0,0 +1,33 @@
+﻿using PersonLibrary;
+using System;
+
+namespace PersonClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            p.FirstName = "anna";
+            p.LastName = "JANSEN";
+            p.Address = "Dorpsstraat 23";
+            p.Age = 18;
+            //Console.WriteLine($"{p.FirstName}\t{p.LastName}\t{p.Address}\t{p.Age}");
+          // Console.WriteLine(p.GetDetails());
+
+            Person q = new Person("BOB","bouw","Kerkstraat 45",34);
+            //q.FirstName = "BOB";
+            //q.LastName = "bouw";
+            //q.Address = "Kerkstraat 45";
+            //q.Age = 34;
+           // Console.WriteLine(q.GetDetails());
+
+            Person[] personArr = { p, q };
+            foreach (var item in personArr)
+            {
+                Console.WriteLine(item.GetDetails());
+            }
+
+        }
+    }
+}
