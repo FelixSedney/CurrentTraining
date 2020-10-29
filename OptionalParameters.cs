d541865d476a984879c1bb9d3f4698250cf29464
FelixSedney <30689523+FelixSedney@users.noreply.github.com>
2020-10-29 10:16:03 +0100

    Add files via upload

---
+++ OptionalParameters.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace OptionalParametersDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Power(12));
+            Console.WriteLine(Power(10,3));
+        }
+
+      
+
+
+        //static int Power(int x)
+        //{
+        //    return x * x;
+        //}
+
+        static int Power(int x, int y=2)//wordt y niet opgegeven vul dan zelf een 2 in
+        {
+            int result = 1;
+            for (int i = 0; i < y; i++)
+            {
+                result *= x;
+            }
+            return result;
+        }
+    }
+}
---
+++ TupleDemo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TupleDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = DateNextWeek();
+            Console.WriteLine($"next week is {result.d}-{result.m}");
+        }
+
+       // return is een tuple
+        static  (int d,int m)DateNextWeek()//return the day and the month
+        {
+            DateTime today = DateTime.Now;
+            DateTime result=  today.AddDays(7);
+            return (result.Day, result.Month);
+        }
+
+    }
+}
