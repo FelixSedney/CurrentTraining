ab069739deda20888d63bb1d7c5cb38241aa22c2
FelixSedney <30689523+FelixSedney@users.noreply.github.com>
2020-10-30 11:16:20 +0100

    Add files via upload

---
+++ PersonClient2.cs
@@ -0,0 +1,49 @@
+﻿using PersonLibray;
+using System;
+using System.Collections.Generic;
+
+namespace PersonClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person("anna","jansen",21,"Kerkstraat 12");
+            
+            Person q = new Person();
+            q.FirstName = "BOB";
+            q.LastName = "slager";
+            q.Address = "Schoolstraat 23";
+            q.Age = 34;
+
+
+            Student s1 = new Student("VERA", "schilder", 34, "Kortesteeg 45", "Mathematics", 2);
+            Student s2 = new Student("ErMa", "TIMMERMAN", 34, "whatever 67", "Databases", 2);
+            Student s3 = new Student("Gerard", "MeTsElAaR", 34, "Bloemensteeg 41235", "Development", 2);
+
+
+            //Person[] personArr = { p, q, new Person("otto", "BaKkEr", 45, "Langestraat 67"),s1,s2,s3 };
+            List<Person> personArr = new List<Person>() { p, q, new Person("otto", "BaKkEr", 45, "Langestraat 67"), s1, s2};
+            
+            personArr.Add(s3);//List<T> is een dynamische collectie waar een array juist statisch
+            foreach (var item in personArr)
+            {
+                Console.WriteLine(item.GetDetails());
+            }
+
+            foreach (var item in personArr)
+            {
+                if (item is Student)//is van het type
+                {
+                    ((Student)item).Grade += 1;
+                }
+            }
+            Console.WriteLine();
+            // Console.WriteLine(p);
+            foreach (var item in personArr)
+            {
+                Console.WriteLine(item.GetDetails());
+            }
+        }
+    }
+}
---
+++ PersonLibrary2_Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace PersonLibray
+{
+    public   class Person//mogelijkheden zijn ABSTRACT, STATIC en SEALED
+    {
+        private string _firstName;
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = $"{value.Substring(0,1).ToUpper()}{value.Substring(1).ToLower()}"; }
+        }
+
+        private string _lastName;
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = $"{value.Substring(0, 1).ToUpper()}{value.Substring(1).ToLower()}"; }
+        }
+        public int Age { get; set; }
+        public string Address { get; set; }
+
+        public Person() { }
+
+        public Person(string firstName,string lastName,int age, string address)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+            Age = age;
+        }
+
+        public virtual string GetDetails()
+        {
+            return $"{FirstName}\t{LastName}\t{Address}\t{Age}";
+        }
+
+        public override string ToString()
+        {
+            //return base.ToString();
+            return $"{FirstName}\t{LastName}\t{Address}\t{Age}";
+        }
+
+    }
+}
